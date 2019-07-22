51bdb0ffc60738cb39490822be39d0fde3571b4e
Kaylyn Kwak <kkwak0803@gmail.com>
2019-07-22 09:49:29 -0700

    CRUD actions for ideas

---
+++ quiz_2/idea_factory/app/controllers/ideas_controller.rb
@@ -0,0 +1,49 @@
+class IdeasController < ApplicationController
+    before_action :find_id, only: [:show, :destroy, :edit, :update]
+
+    def new
+        @idea = Idea.new
+    end
+
+    def create
+        @idea = Idea.new idea_params
+        if @idea.save
+            redirect_to @idea, notice: "New idea submitted!"
+        else
+            redirect_to ideas_path, alert: "Unable to create a new idea!"
+        end
+    end
+
+    def index
+        @ideas = Idea.order(created_at: :desc)
+    end
+
+    def show
+    end
+    
+    def destroy
+        @idea.destroy
+        redirect_to ideas_path, notice: "Idea deleted!"
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @idea.update idea_params
+            redirect_to @idea, notice: "Idea Updated!"
+        else
+            redirect_to ideas_path, alert: "Unable to update your idea. Try again!"
+        end
+    end
+
+    private
+
+    def idea_params
+        params.require(:idea).permit(:title, :description)
+    end
+
+    def find_id
+        @idea = Idea.find params[:id]
+    end
+end
